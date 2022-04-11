ccbd58843d99825fee8f5884a46c897a87786778
melkesy <melkesy1@gmail.com>
2022-04-11 14:45:30 -0700

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,19 @@ int main(void)
 
 {
 
-int digit1, digit2;
+int i, j;
 
-for (digit1 = 0; digit1 < 9; digit1++)
+for (i = 0; i < 9; i++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+for (j = i + 1; j < 10; j++)
 {
-putchar((digit1 % 10 + '0');
-putchar((digit2 % 10 + '0');
+putchar((i % 10 + '0');
+putchar((j % 10 + '0');
 
-if (digit1 == 8 && digit2 == 9)
+if (i == 8 && j == 9)
 continue;
 
-putchar(';');
+putchar(',');
 putchar(' ');
 }
 }
