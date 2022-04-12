10ff5b7f66ef7c2dff2c081b56d2428b5ea14cee
melkesy <melkesy1@gmail.com>
2022-04-12 04:17:37 -0700

    14th

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,18 +5,17 @@
 */
 int main(void)
 {
-unsigned long count, i, j, k;
 
+unsigned long count, i, j, k;
 i = 0;
 j = 1;
-
 for (count = 0; count < 50; count++)
 {
- k = i + j;
- i = j;
- printf("%lu", k);
+k = i + j;
+i = j;
+printf("%lu", k);
 if (count == 49)
- putchar('\n');
+putchar('\n');
 else
 {
 printf(", ");
